c1da9d32fba7ccebe8757e672eb41bcc8b2eced8
Waleed <waleedghaleb@gmail.com>
2025-11-15 20:18:01 -0800

    testing

--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -7,3 +7,10 @@ use_step_matcher('re')
 def step_impl(context):
     browser = webdriver.Chrome()
     browser.get('http://127.0.0.1:5000')
+
+
+@then('I should see the blog page')
+def step_impl(context):
+    expected_url = 'http://127.0.0.1:5000'
+    browser = webdriver.Chrome()
+    assert browser.current_url == expected_url
